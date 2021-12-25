6e86033fa3b6eea23eccf32b2cd9ecb6c8073c46
driphxney <lgswnigga@mail.ru>
2021-12-25 13:10:20 +0300

    Решение с тремя проектами #1

---
+++ Task 1/Task 1-2/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+/**
+ * \brief Эта функция выполняет вычисления по заданной формуле
+ * \param a параметр a
+ * \param b параметр b
+ * \return значение функции
+  */
+double getA(double x, double y);
+
+/**
+ * \brief Эта функция выполняет вычисления по заданной формуле
+ * \param a параметр a
+ * \param b параметр b
+ * \return значение функции
+  */
+double getB(double x, double y);
+
+/**
+ * \brief Точка входа в программу
+ * \return 0 в случае успеха
+ */
+
+int main()
+{
+	double x = 0;
+	double y = 0;
+	std::cin >> x;
+	std::cin >> y;
+
+	const double a = getA(x, y);
+	const double b = getB(x, y);
+
+	std::cout << "x = " << x << " y = " << y << " Arithmetic mean = " << a << " Geometric mean = " << b << std::endl;
+    return 0;
+}
+
+double getA(double x, double y)
+{
+	return (x + y) / 2;
+}
+
+double getB(double x, double y)
+{
+	return sqrt(fabs(x * y));
+}
