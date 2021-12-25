25f753e838af826da9aaa1d1b255b738a1f99c1b
Marina Paley <marina_paley@mail.ru>
2021-12-26 00:29:53 +0300

    Решение с тремя проектами #1

---
+++ Task 1/Task 1-2/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+/**
+ * \brief Точка входа в программу.
+ * \return Код ошибки (0 – в случае успеха).
+ */
+int main()
+{
+    std::cout << "Task 1-2" << std::endl;
+
+    return 0;
+}
