6f0d5762b618804da4f46ded333d1c7c83662d0e
Vadim Molotkov <81369917+driphxney@users.noreply.github.com>
2022-02-24 23:25:24 +0300

    Решена задача 4-1

--- Task 4/Task 4-1/main.cpp
+++ Task 4/Task 4-1/main.cpp
@@ -1,200 +1,154 @@
 #include <iostream>
-#include <sstream>
 #include <random>
 
 using namespace std;
 
 /**
- * \brief Перечислимый тип способов задания массива.
+ * \brief Ввод элементов массива с клавиатуры.
+ * \param a - Указатель на массив.
+ * \param n - Размер массива.
  */
-enum class input_types
-{
+void FillArrayKeyboard(int* a, const size_t n);
+
+/**
+ * \brief Заполнение элементов массива случайными числами.
+ * \param a - Указатель на массив.
+ * \param n - Размер массива.
+ * \param min - Левая граница диапазона случайных чисел.
+ * \param max - Правая граница диапазона случайных чисел.
+ */
+void FillArrayRandom(int* a, const size_t n, const int min, const int max);
+
+/**
+ * \brief Вывод элементов массива в консоль.
+ * \param a - Указатель на массив.
+ * \param n - Размер массива.
+ */
+void PrintArray(const int* a, const size_t n);
+
+/**
+ * \brief Подсчет суммы четных элементов массива, больших заданного числа.
+ * \param a - Указатель на массив.
+ * \param n - Размер массива.
+ * \param c - Заданное число.
+ * \return Искомая сумма.
+ */
+int SumTask(const int* a, const size_t n, const int c);
+
+/**
+ * \brief Перемножение четных положительных элементов массива на значение последнего элемента.
+ * \param a - Указатель на массив.
+ * \param n - Размер массива.
+ */
+void MultLast(int* a, const size_t n);
+
+/**
+ * \brief Нахождение индекса первой пары элементов массива противоположных знаков.
+ * \param a - Указатель на массив.
+ * \param n - Размер массива.
+ * \return Индекс первого элемента искомой пары.
+ */
+int PosPair(const int* a, const size_t n);
+
+/**
+ * \brief Перечислимый тип методов заполнения.
+ */
+enum class FillMethod { //перечислимый тип
     /**
-     * \brief ввод вручную.
+     * \brief Заполнение элементов массива с клавиатуры.
      */
-    MANUALLY,
+    KEYBORD = 0,
 
     /**
-     * \brief создание с использованием генератора случайных чисел.
+     * \brief Заполнение элементов массива случайными числами.
      */
-     RANDOMLY
+     RANDOM = 1
 };
 
-/**
- * \brief Метод заполнения массива
- * \param size Размер массива
- * \param input_type Способ создания массива
- * \param LOWER_BOUND Нижняя граница для случайных чисел
- * \param UPPER_BOUND Верхняя граница для случайных чисел
- * \return Заполненный массив
- */
-int* get_array(const size_t size, const int input_type, const int LOWER_BOUND, const int UPPER_BOUND);
-
-/**
- * \brief Функция для получения размера массива
- * \param message Мотивоционное сообщение для пользователя
- * \return Размер массива
- */
-size_t get_size(const std::string& message);
-
-/**
- * \brief Вывод в строку элементов массива
- * \param size Размер массива
- * \return Строка с элементами
- */
-string to_string(const int*, const size_t size);
-
-/**
- * \brief Находит произведение четных элементов, значение которых по модулю меньше 5
- * \param size Размер массива
- * \return Произведение элементов
- */
-int get_even_composition(int*, const size_t size);
-
-/**
- * \brief Находит количество нечетных элементов по модулю превышающих заданное число (\a A)
- * \param size Размер массива
- * \param A Число A
- * \return Количество элементов
- */
-int get_odd_quantity(int*, const size_t size, const int A);
-
-/**
- * \brief Умножает все нечетные элементы массива кратные 3 на их номер
- * \param size Размер массива
- * \return Количество элементов
- */
-void multiply_elements(int*, const size_t size);
-
-
-/**
- * \brief Точка входа в программу
- * \return 0 в случае успеха
- */
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    auto error_code = 0;
-    int* my_array = nullptr;
-    const int LOWER_BOUND = -10;
-    const int UPPER_BOUND = 20;
-    try
-    {
-        const auto size = get_size("Введите размер массива = ");
-        cout << "Выберите способ создания массива: " << static_cast<int>(input_types::MANUALLY) << " - вручную, " << static_cast<int>(input_types::RANDOMLY) << " - заполнить случайными числами ";
-        int input_type;
-        cin >> input_type;
-        my_array = get_array(size, input_type, LOWER_BOUND, UPPER_BOUND);
-        cout << "Полученный массив:\n";
-        cout << to_string(my_array, size);
-        cout << "\nПроизведение четных элементов, значение которых по модулю меньше 5 = " << get_even_composition(my_array, size) << '\n';
-        cout << "Введите число A = ";
-        int A;
-        cin >> A;
-        cout << "Количество нечетных элементов по модулю превышающих заданное число (A) = " << get_odd_quantity(my_array, size, A) << '\n';
-        cout << "Массив после умножаения всех нечетных элементов массива кратных 3 на их номер:\n";
-        multiply_elements(my_array, size);
-        cout << to_string(my_array, size);
-    }
-    catch (exception& e)
-    {
-        cout << e.what();
-        error_code = 1;
-    }
+    setlocale(LC_ALL, "rus");
+    size_t n, key;
+    FillMethod fm;
+    cout << "Введите размер массива: ";
+    cin >> n;
+    int* a = new int[n];
 
-    if (my_array != nullptr)
-    {
-        delete[] my_array;
-        my_array = nullptr;
+    do {
+        cout << "\nЗаполнить массив:\n - 0. С клавиатуры\n - 1. Случайными числами\n\nВведите опцию: ";
+        cin >> key;
+    } while (key < 0 || key>1);
+    fm = FillMethod(key);
+
+    if (fm == FillMethod::KEYBORD)
+        FillArrayKeyboard(a, n);
+    else
+        FillArrayRandom(a, n, -100, 200);
+
+    PrintArray(a, n);
+
+    int c;
+    cout << "Введите заданное число: ";
+    cin >> c;
+    cout << "Сумма четных элементов, больших заданного числа: " << SumTask(a, n, c) << endl;
+
+    cout << "Умножаем все четные положительные на последний элемент..." << endl << endl;
+    MultLast(a, n);
+    PrintArray(a, n);
+
+    int d = PosPair(a, n);
+    if (d == -1)
+        cout << "Пары элементов с разными знаками не найдено" << endl;
+    else
+        cout << "Индекс пары элементов с разными знаками: " << d << endl;
+
+    if (a != nullptr){
+        delete[] a;
+        a = nullptr;
     }
-    return error_code;
+    return 0;
 }
 
-size_t get_size(const std::string& message)
-{
-    int size = -1;
-    std::cout << message;
-    std::cin >> size;
-
-    if (size < 0)
-    {
-        throw std::out_of_range("Incorrect size. Value has to be greater or equal zero.");
+void FillArrayKeyboard(int* a, const size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        cout << "a[" << i << "]: ";
+        cin >> a[i];
     }
-
-    return size;
+    cout << endl;
 }
 
-int* get_array(const size_t size, const int input_type, const int LOWER_BOUND, const int UPPER_BOUND)
-{
-    if (size == 0)
-        throw out_of_range("Неправильный размер массива");
+void FillArrayRandom(int* a, const size_t n, const int min, const int max) {
+    random_device rd;
+    mt19937 gen(rd());
+    const uniform_int_distribution<int> dist(min, max);
 
-    const auto array = new int[size];
-    //Will be used to obtain a seed for the random number engine
-    std::random_device rd;
-
-    //Standard mersenne_twister_engine seeded with rd()
-    std::mt19937 gen(rd());
-    const std::uniform_int_distribution<> uniformIntDistribution(LOWER_BOUND, UPPER_BOUND);
-    for (size_t index = 0; index < size; index++)
-    {
-        switch (input_type)
-        {
-        case static_cast<int>(input_types::MANUALLY):
-        {
-            cout << "Введите " << index + 1 << " элемент массива = ";
-            cin >> array[index];
-            break;
-        }
-        case static_cast<int>(input_types::RANDOMLY):
-        {
-            array[index] = uniformIntDistribution(gen);
-            break;
-        }
-        default:
-            cout << "Ошибка!";
-        }
-    }
-
-    return array;
+    for (size_t i = 0; i < n; i++)
+        a[i] = dist(gen);
 }
 
-string to_string(const int* array, const size_t size)
-{
-    if (array == nullptr)
-        throw invalid_argument("Массив не существует");
-
-    stringstream buffer;
-    buffer << "{";
-    for (size_t index = 0; index < size - 1; index++)
-    {
-        buffer << array[index] << ", ";
-    }
-    buffer << array[size - 1] << "}";
-    return buffer.str();
+void PrintArray(const int* a, const size_t n) {
+    for (size_t i = 0; i < n; i++)
+        cout << "a[" << i << "]: " << a[i] << endl;
+    cout << endl;
 }
 
-int get_even_composition(int* array, const size_t size) {
-    int comp = 1;
-    for (size_t i = 0; i < size; i++) {
-        if ((abs(array[i]) < 5) && (array[i] % 2 == 0))
-            comp *= array[i];
-    }
-    return comp;
+int SumTask(const int* a, const size_t n, const int c) {
+    int sum = 0;
+    for (size_t i = 0; i < n; i++)
+        if (a[i] % 2 == 0 && a[i] > c)
+            sum += a[i];
+    return sum;
 }
 
-int get_odd_quantity(int* array, const size_t size, const int A) {
-    int quan = 0;
-    for (size_t i = 0; i < size; i++) {
-        if ((abs(array[i]) > A) && (array[i] % 2 != 0))
-            quan++;
-    }
-    return quan;
+void MultLast(int* a, const size_t n) {
+    for (size_t i = 0; i < n; i++)
+        if (a[i] % 2 == 0 && a[i] > 0)
+            a[i] *= a[n - 1];
 }
 
-void multiply_elements(int* array, const size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        if (array[i] % 3 == 0)
-            array[i] *= i;
-    }
+int PosPair(const int* a, const size_t n) {
+    for (size_t i = 0; i < n - 1; i++)
+        if (a[i] * a[i + 1] < 0)
+            return i;
+    return -1;
 }
