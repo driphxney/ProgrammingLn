36e34a7573f22f3c0efd4eb8f8507167144aa072
Vadim Molotkov <81369917+driphxney@users.noreply.github.com>
2022-02-25 16:30:36 +0300

    Решена задача 2-1

--- Task 2/Task 2-1/main.cpp
+++ Task 2/Task 2-1/main.cpp
@@ -1,6 +1,106 @@
-﻿#include <iostream>
+#include <iostream>
+#include <string>
 
-int main()
+using namespace std;
+
+/**
+ * \brief Перечисляемый тип вычеслений, для которых считаем площадь и объем
+ */ 
+enum class Cube
 {
+	none,square,area,capacity,
+};
+
+/**
+ * \brief Функция расчета площади грани куба
+ * \param a сторона куба
+ * \return Площадь гарни куба
+ */
+double GetCubeSquare(const double a);
+
+/**
+ * \brief Функция расчета площади полной поверхности куба
+ * \param a сторона куба
+ * \return Площадь полной поверхности куба
+ */
+double GetCubeArea(const double a);
+
+/**
+ * \brief Функция расчета объема куба
+ * \param a сторона куба
+ * \return Объем куба
+ */
+double GetCubeCapacity(const double a);
+
+/**
+ * \brief Ввод стороны куба
+ * \param message Разъясняющая надпись
+ * \param out Произвольный поток вывода
+ * \param in Произвольный поток ввода
+ * \return Сторону куба
+ */
+double ReadSide(const string& message = "", ostream& out = cout, istream& in = cin);
+
+
+/**
+ * \brief 
+ * \param message Сообщение для пользователя
+ * \param out Произвольный поток вывода
+ * \param in Произвольный поток ввода
+ * \return Выбор пользователя
+ */
+Cube ReadUserChoice(const string& message = "", ostream& out = cout, istream& in = cin);
+
+/**
+ * \brief Точка вход в программу
+ * \return Код ошибки, если 0 - успешное выполнение
+ */
+int main() 
+{
+	setlocale(LC_ALL, "Russian");
+	
+	const auto message = "Выберите, что посчитать: "
+	    + to_string(static_cast<int>(Cube::square)) + " - площадь грани,"
+		+ to_string(static_cast<int>(Cube::area)) + " - площадь полной поверхности,"
+		+ to_string(static_cast<int>(Cube::capacity)) + " - объем";
+		
+	const auto cube = ReadUserChoice(message);
+	
+	switch (cube) 
+  {
+		case Cube::square: 
+    {
+			const auto side = ReadSide("Введите сторону куба = ");
+			
+			const auto squareCube = GetCubeSquare(side);
+			cout << "\nПлощадь грани куба " << squareCube << endl;
+			break;
+		}
+		case Cube::area: 
+    {
+			const auto side = ReadSide("Введите сторону куба = ");
+			
+			const auto areaCube = GetCubeArea(side);
+			cout << "\nПлощадь полной поверхности " << areaCube << endl;
+			break;
+		}
+		case Cube::capacity: 
+    {
+			const auto side = ReadSide("Введите сторону куба = ");
+			
+			const auto capacityCube = GetCubeCapacity(side);
+			cout << "\nОбъем " << capacityCube << endl;
+			break;
+		}
+	default:
+	    cout << "ошибка!";
+	}
+	
+	system("pause");
 	return 0;
+}
+
+double GetCubeSquare(const double a) 
+{
+	return a * a;
 }
